4b7b18b548ebad3fb5726ce1a87dbb9586b495fb
Camila Zanata <camilazanata1999@gmail.com>
2022-05-31 22:38:41 -0300

    Challenge Theme Switcher

---
+++ ThemeSwitcher/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+  
+    checkbox.addEventListener('change', function () {
+      if (checkbox.checked) {
+        document.body.classList.add('dark-theme');
+      } else {
+        document.body.classList.remove('dark-theme');
+      }
+    });
+  });
